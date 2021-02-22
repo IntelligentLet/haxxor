8c2a9df88364682286bda5227ccad792e73cf687
LogicGo <logicgo@protonmail.com>
2021-02-21 19:43:18 -0500

    ban reeeeeaaaally young accounts (spam)

--- commands/join.js
+++ commands/join.js
@@ -2,11 +2,12 @@ module.exports = {
     name: 'join',
     description: 'when user joins guild',
     execute(member, client) {
+        if (Date.now() - member.user.createdAt < 1000*60*60*24*7) {
+            member.send("Your account was banned for being too young (< 1 week). This is to prevent spam.")
+            member.ban()
+            return
+        }
         client.channels.cache.get(process.env.WELCOMECHANNEL)
-            .send(
-                `Welcome ${member.user}! You are member #${(client.guilds.cache.get(member.guild.id).memberCount)}. Please read ${client.channels.cache.get(process.env.RULES)} to get started!
-                `)
-        member.roles
-            .add(member.guild.roles.cache.find(role => role.id === process.env.JOINROLE)).catch()
+            .send(`Welcome ${member.user}! You are member #${(client.guilds.cache.get(member.guild.id).memberCount)}. Please read ${client.channels.cache.get(process.env.RULES)} to get started!`)
     },
 };
